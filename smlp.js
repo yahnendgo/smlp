7dd64d5685439956a89795353382e71372761529
YahNé Ndgo <yndgo@bennington.edu>
2015-10-16 17:03:14 +0200

    added links to pages, and some other stuff

--- smlp.js
+++ smlp.js
@@ -3,10 +3,37 @@ var whenReady = function (){
 	$(".button").hover(function(){
 		console.log("hide");
 		$("#login").hide();
-	},
-	function(){
+	}, function(){
 		$("#login").show();
 	})
+
+
+function submit(){
+	console.log("submit");
+	var password=document.getElementById("ptext").value;
+	var username=document.getElementById("text").value;
+	if (password == "WeRuleTheWorld!!!"){
+		console.log(username);
+		return false;
+	} else {
+		console.log("else");
+		return false;
+	}
+	return false;
+}
+
+	// $("button").onClick(function(){
+	// 	if (".ptext" == "WeRuleTheWorld!!!") {
+	// 		console.log("input.Username");
+	// 	} else {
+	// 		$("body").hide(10000);
+	// 		<a>.fadeIn()
+	// }
+	// 	});
+
+	$("#login-form").submit(submit)
+
+
 }
 
 $(document).ready(whenReady);
